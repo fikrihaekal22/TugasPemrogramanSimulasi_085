936c3af81d73be0fe2c3c40064dd22679f69a693
fikrihaekal22 <67132372+fikrihaekal22@users.noreply.github.com>
2021-03-01 21:36:38 +0700

    update 3

--- Tugas1_PemSimulasi_152018085/korelasi sederhana/pearsoncorrelation.py
+++ Tugas1_PemSimulasi_152018085/korelasi sederhana/pearsoncorrelation.py
@@ -1,75 +1,97 @@
-import math
+# PROGRAM KORELASI HAEKAL#
 
 
-def pearson_correlation(independent, dependent):
+# -----------------------------------------#
 
-    """
-   Menerapkan Korelasi Pearson, menggunakan beberapa fungsi utilitas untuk
-     hitung nilai antara sebelum menghitung dan mengembalikan rho
-    """
+#penjelasan membuat fungsi untuk mengambil nilai dan menghitung nilai,
 
-    # covariance
-    independent_mean = _arithmetic_mean(independent)
-    dependent_mean = _arithmetic_mean(dependent)
-    products_mean = _mean_of_products(independent, dependent)
-    covariance = products_mean - (independent_mean * dependent_mean)
+#ngitung 1 nilai
+def nilai1(inp):
+    arr = []
+    out = 0
+    for x in range(n):
+        arr.append(inp[x])
+        out += arr[x]
+    return out, arr
 
-    # standard deviations of independent values
-    independent_standard_deviation = _standard_deviation(independent)
+#ngitung 2 nilai
+def nilai2(inpX, inpY):
+    arr = []
+    out = 0
+    for x in range(n):
+        arr.append((inpX[x] * inpY[x]))
+        out += arr[x]
+    return out, arr
 
-    # standard deviations of dependent values
-    dependent_standard_deviation = _standard_deviation(dependent)
+#ngitung kuadrat nilai
+def nilaipow1(inp):
+    arr = []
+    out = 0
+    #untuk menghitung nilainya
+    for x in range(n):
+        arr.append((inp[x] ** 2))
+        out += arr[x]
+    return out, arr
 
-    # Pearson Correlation Coefficient
-    rho = covariance / (independent_standard_deviation * dependent_standard_deviation)
+#operasi perhitungan
+def cariHasil(inpX, inpY, inpN):
+    Yi = nilai1(inpY)[0]
+    Yi2 = nilaipow1(inpY)[0]
+    Xi2 = nilaipow1(inpX)[0]
+    Xi = nilai1(inpX)[0]
+    XiYi = nilai2(inpX, inpY)[0]
+    out = round(((inpN * XiYi) - (Xi * Yi)) / ((((inpN * Xi2) - (Xi ** 2)) * ((inpN * Yi2) - (Yi ** 2))) ** 0.5), 3)
+    return out
+# NRP : 152018085
 
-    return rho
-
-
-def  _arithmetic_mean(data):
-
-    """
-   Total / hitung: arti sehari-hari dari "rata-rata"
-    """
-
-    total = 0
-
-    for i in data:
-        total+= i
-
-    return total / len(data)
-
-
-def  _mean_of_products(data1, data2):
-
-    """
-   Rata-rata produk dari nilai yang sesuai dari data bivariat
-    """
-
-    total = 0
-
-    for i in range(0, len(data1)):
-        total += (data1[i] * data2[i])
-
-    return total / len(data1)
+#pengkategorian dari hasil nilai r
+def skalaguilford(inp):
+    if (inp < 0.2) and (inp >= 0):
+        skala = 'Sangat Lemah'
+    elif (inp < 0.4) and (inp >= 0.2):
+        skala = 'Lemah'
+    elif (inp < 0.6) and (inp >= 0.4):
+        skala = 'Sedang'
+    elif (inp < 0.8) and (inp >= 0.6):
+        skala = 'Baik'
+    elif (inp <= 1.0) and (inp >= 0.8):
+        skala = 'Sangat Baik'
+    return skala
 
 
-def  _standard_deviation(data):
+def inputdata():
+    global n
+    print('Inputkan nilai x')
+    while True:
+        entry = input('num : ')
+        if entry == '':
+            break
+        try:
+            x.append(int(entry))
+        except:
+            print("Nilai tidak valid!")
+    print('Inputkan nilai y')
+    while True:
+        entry = input('number : ')
+        if entry == '':
+            break
+        try:
+            y.append(int(entry))
+        except:
+            print("Nilai tidak valid!")
+    n = int(input('Inputkan jumlah nilai : '))
 
-    """
-   Ukuran tentang bagaimana nilai individu biasanya berbeda dari mean_of_data.
-     Akar kuadrat dari varians
-    """
 
-    squares = []
+'''__MAIN PROGRAM__'''
+x = []
+y = []
+n = 0
+inputdata()
+print('====Hasil====')
+hasil = cariHasil(x, y, n)
+print('Skala angka = ', hasil)
+print('Skala Guilford =', skalaguilford(hasil))
+print('Jenis Koefisien Determinasi =', (hasil ** 2) * 100, '%')
+print('Ditemukan', 100 - ((hasil ** 2) * 100), '% kontribusi dari faktor lain')
 
-    for i in data:
-        squares.append(i ** 2)
-
-    mean_of_squares = _arithmetic_mean(squares)
-    mean_of_data = _arithmetic_mean(data)
-    square_of_mean = mean_of_data ** 2
-    variance = mean_of_squares - square_of_mean
-    std_dev = math.sqrt(variance)
-
-    return std_dev
+#Tugas Pemrograman Simulasi
--- Tugas1_PemSimulasi_152018085/regresi_sederhana.py
+++ Tugas1_PemSimulasi_152018085/regresi_sederhana.py
@@ -1,23 +1,87 @@
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn.linear_model import LinearRegression
-df = pd.DataFrame([[151,63],[174,81],[138,56],[186,91],[128,47],[136,57],[179,76],[163,72],[152,62],[131,48]])
-df.columns = ['x', 'y']
-x_train = df['x'].values[:,np.newaxis]
-y_train = df['y'].values
-lm = LinearRegression()
-lm.fit(x_train,y_train) #fase training
-print('Coefficient : ' + str(lm.coef_))
-print('Intercept : ' + str(lm.intercept_))
-x_test = [[170],[171]] #data yang akan diprediksi
-p = lm.predict(x_test) #fase prediksi
-print(p) #hasil prediksi
-#prepare plot
-pb = lm.predict(x_train)
-dfc = pd.DataFrame({'x': df['x'],'y':pb})
-plt.scatter(df['x'],df['y'])
-plt.plot(dfc['x'],dfc['y'],color='red',linewidth=1)
-plt.xlabel('Tinggi dalam cm')
-plt.ylabel('Berat dalam Kg')
-plt.show()
+#TUGAS REGRESI HAEKAL 152018085#
+
+#ngitung 1 nilai
+def nilai1(inp):
+    arr = []
+    out = 0
+    for x in range(n):
+        arr.append(inp[x])
+        out += arr[x]
+    return out, arr
+
+#ngitung 2 nilai
+def nilai2(inpX, inpY):
+    arr = []
+    out = 0
+    for x in range(n):
+        arr.append((inpX[x] * inpY[x]))
+        out += arr[x]
+    return out, arr
+
+#ngitung kuadrat nilai
+def nilaipow1(inp):
+    arr = []
+    out = 0
+    for x in range(n):
+        arr.append((inp[x] ** 2))
+        out += arr[x]
+    return out, arr
+
+
+def cariA(inpX, inpY, inpN):
+    Yi = nilai1(inpY)[0]
+    Xi2, nXi2 = nilaipow1(inpX)[:2]
+    Xi = nilai1(inpX)[0]
+    XiYi = nilai2(inpX, inpY)[0]
+    out = []
+    for x in range(n):
+        out.append(round(((Yi * Xi2) - (Xi * XiYi)) / ((inpN * nXi2[x]) - (Xi ** 2)), 3))
+    return out
+
+def cariB(inpX, inpY, inpN):
+    Yi = nilai1(inpY)[0]
+    Xi = nilai1(inpX)[0]
+    XiYi = nilai2(inpX, inpY)[0]
+    out = []
+    for x in range(n):
+        out.append(round(((inpN * XiYi) - (Xi * Yi)) / ((inpN * Xi) - (Xi ** 2)), 3))
+    return out
+
+def inputdata():
+    global n
+    print('Inputkan nilai a (konstanta)')
+    while True:
+        entry = input('number : ')
+        if entry == '':
+            break
+        try:
+            x.append(int(entry))
+        except:
+            print("Nilai tidak valid!")
+    print('Inputkan nilai b (koefisien)')
+    while True:
+        entry = input('number : ')
+        if entry == '':
+            break
+        try:
+            y.append(int(entry))
+        except:
+            print("Nilai tidak valid!")
+    n = int(input('nilai n : '))
+
+
+
+'''__MAIN PROGRAM__'''
+x = []
+y = []
+n = 0
+inputdata()
+a = cariA(x,y,n)
+b = cariB(x,y,n)
+print('Nilai a (konstatnta) = ', a)
+print('Nilai b (koefisien) = ', b)
+print('===Bentuk Persamaan===')
+for i in range(n):
+    print('(', i+1 ,') Y = ', a[i], ' + (', b[i], ')x')
+
+    #Tugas 1 Pemrograman Simulasi
